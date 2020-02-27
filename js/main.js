0ea0d2f8f88eed8de3abfed8e6de841c59df2766
Anastasia <anastasiavyalt@gmail.com>
2020-02-27 19:12:36 +0700

    Сделала попап с формой обратной связи

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 /* SLIDERS */
-console.log("ffff");
 function Slider(slidesClass, bulletsClass, prevClass, nextClass) {
     this.index = 0;
     this.slidesClass = slidesClass;
@@ -56,3 +55,43 @@ promoSlider.showSlides(0);
 
 let servicesSlider = new Slider(".service-slide", ".services-slider-button");
 servicesSlider.showSlides(0);
+
+/* POPUPS */
+let contactLink = document.querySelector(".popup-contact-link");
+let contactPopup = document.querySelector(".popup-contact-us");
+let contactForm = document.querySelector(".contact-us-form");
+let contactName = contactForm.querySelector("#name-surname");
+let contactEmail = contactForm.querySelector("#email");
+let contactText = contactForm.querySelector("#text");
+let contactClose = contactPopup.querySelector(".modal-close");
+
+contactLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contactPopup.classList.add("modal-show");
+    contactName.focus();
+    console.log(contactPopup);
+});
+
+contactClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contactPopup.classList.remove("modal-show");
+    contactPopup.classList.remove("contact-us-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (contactPopup.classList.contains("modal-show")) {
+        contactPopup.classList.remove("modal-show");
+      }
+    }
+});
+
+contactForm.addEventListener("submit", function (evt) {
+    if (!contactName.value || !contactEmail.value || !contactText.value) {
+      evt.preventDefault();
+      contactPopup.classList.remove("contact-us-error");
+      contactPopup.offsetWidth = contactPopup.offsetWidth;
+      contactPopup.classList.add("contact-us-error");
+    } 
+});
